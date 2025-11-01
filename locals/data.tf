07c943cea8b0a397e3b276e1bec252f14e39b0c5
venkata.perumalla <venkata.perumalla@pragmaticplay.com>
2025-11-01 22:36:30 +0530

    terraform

---
+++ locals/data.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "joindevops" {
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "ami_id" {
+    value = data.aws_ami.joindevops.id
+}
---
+++ locals/variables.tf
@@ -0,0 +1,63 @@
+variable "project" {
+    default = "roboshop"
+}
+variable "environment" {
+    default = "dev"  
+}
+
+# variable "common_name" {
+#   default = "${var.project}-${var.environment}"
+# }
+
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map
+    default = {
+        Name = "terraform-demo"
+        Terraform = "tue"
+        Project = "Join DevOps"
+        Environment = "Dev"
+    }
+}
+
+variable "sg_name" {
+    type = string
+    default = "allow_all"
+    description = "Security Group Name to attach ec2 instance"  # optional
+}
+
+variable "cidr" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "ingress_from_port" {
+    type = number
+    default = 0
+}
+
+variable "ingress_to_port" {
+    default = 0
+}
+
+variable "egress_from_port" {
+    default = 0
+}
+
+variable "egress_to_port" {
+    default = 0
+}
+
+variable "protocol" {
+    type = string
+    default = "-1"
+}
